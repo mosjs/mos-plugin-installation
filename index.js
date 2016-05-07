684af1be21d367d87896581fc5e083df47127877
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-05-07 22:04:15 +0300

    fix: the installation function works when no options passed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const renderInstallation = require('./lib/render-installation')
 
 function plugin (markdown) {
   return {
-    installation: opts => renderInstallation(Object.assign(opts, {
+    installation: opts => renderInstallation(Object.assign(opts || {}, {
       pkg: markdown.pkg,
     })),
   }
